19a982d51d2c96ea7951be0685691d387fa4712d
Divir Gupta <divir94@gmail.com>
2017-08-01 01:24:55 -0400

    gdax websocket initial commit

--- bitcoin/bot/order_book.py
+++ bitcoin/bot/order_book.py
@@ -40,19 +40,55 @@ def get_price_levels_from_level_2(lst):
     return result
 
 
+def add_order(levels, price, size, order_id):
+    idx = levels.bisect_key_left(price)
+    seen_price = idx < len(levels) and levels[idx].price == price
+    if seen_price:
+        price_level = levels[idx]
+        price_level.size += size
+        price_level.orders[order_id] = size
+    else:
+        price_level = PriceLevel(price, size, {order_id: size})
+        levels.add(price_level)
+    return
+
+
+def remove_order(levels, price, new_size, order_id):
+    """remove (partially) filled or cancelled orders"""
+    idx = levels.bisect_key_left(price)
+    price_level = levels[idx]
+    assert order_id in price_level.orders, 'order not in order book!'
+
+    # only order at this price level
+    if len(price_level.orders) == 1:
+        levels.remove(price_level)
+    # just remove this order from the price level
+    else:
+        old_size = price_level.orders[order_id]
+        price_level.size -= old_size
+        price_level.orders.pop(order_id)
+        assert new_size <= old_size, 'new size has to be less than old size!'
+    return
+
+
+def update_order(levels, price, new_size, order_id):
+    idx = levels.bisect_key_left(price)
+    price_level = levels[idx]
+    if order_id not in price_level.orders:
+        return
+
+    old_size = price_level.orders[order_id]
+    price_level.size -= (new_size - old_size)
+    price_level.orders[order_id] = new_size
+    assert new_size <= old_size, 'new size has to be less than old size!'
+    return
+
+
 def get_price_levels_from_level_3(lst):
     levels = SortedListWithKey(key=lambda level: level.price)
     for price, size, order_id in lst:
         price, size = float(price), float(size)
-        idx = levels.bisect_key_left(price)
-        seen_price = idx < len(levels) and levels[idx].price == price
-        if seen_price:
-            price_level = levels[idx]
-            price_level.size += size
-            price_level.orders[order_id] = size
-        else:
-            price_level = PriceLevel(price, size, {order_id: size})
-            levels.add(price_level)
+        add_order(levels, price, size, order_id)
     return levels
 
 
@@ -70,41 +106,3 @@ def get_order_book(data, level, _id=None):
     _id = data.get(_id)
     book = OrderBook(bids, asks, _id)
     return book
-
-
-if __name__ == '__main__':
-    lst = [
-        [
-            "2845.18",
-            "0.20265626",
-            "cc6d1639-fa53-4968-abc5-716f2748ee85"
-        ],
-        [
-            "2845.18",
-            "0.0140202",
-            "c9ffaeb2-3c87-40ec-ada7-e242e9192bdc"
-        ],
-        [
-            "2845.15",
-            "0.82691468",
-            "54276351-d0ed-4c8f-9a6b-84141e9751a2"
-        ],
-        [
-            "2845.1",
-            "0.01",
-            "a2f3d6d5-75da-486e-af74-bb14bbbde29f"
-        ]
-    ]
-    import requests
-    import json
-    import time
-
-    url = 'https://www.bitstamp.net/api/v2/order_book/btcusd'
-    #url = 'https://api.gdax.com/products/BTC-USD/book?level=3'
-    response = requests.get(url)
-    data = json.loads(response.content)
-    start = time.time()
-    book = get_order_book(data, 2, 'sequence')['bids']
-    end = time.time() - start
-    print book
-    print end
--- bitcoin/websocket/gdax.py
+++ bitcoin/websocket/gdax.py
@@ -1,42 +1,109 @@
 import json
 import time
-from sortedcontainers import SortedListWithKey
+import requests
 
 from bitcoin.websocket.core import WebSocket
-from bitcoin.bot.order_book import OrderBook, PriceLevel
+import bitcoin.bot.order_book as ob
 
 
-GX_URL = 'wss://ws-feed.gdax.com'
+GX_WS_URL = 'wss://ws-feed.gdax.com'
+GX_HTTP_URL = 'https://api.gdax.com/products/BTC-USD/book'
 
 
 class GdaxOrderBook(WebSocket):
     def __init__(self, on_change):
         channel = {'type': 'subscribe', 'product_ids': ['BTC-USD']}
-        super(GdaxOrderBook, self).__init__(GX_URL, channel)
+        super(GdaxOrderBook, self).__init__(GX_WS_URL, channel)
         self.exchange = 'GDAX'
-        self.book = None
+        self.book = self.reset_book()
+        print 'init'
+        print self.book.id
         self.on_change = on_change
 
+    #TODO(divir): make this aync
+    def reset_book(self):
+        level = 3
+        request = requests.get(GX_HTTP_URL, params={'level': level})
+        data = json.loads(request.content)
+        book = ob.get_order_book(data, level, _id='sequence')
+        return book
+
+    #TODO(divir): queue msgs and apply
     def on_message(self, ws, message):
-        msg = json.loads(message)
+        msg = self.parse_msg(message)
         _type = msg['type']
+        sequence = msg['sequence']
+        print sequence
+
+        if sequence <= self.book.id:
+            # ignore older messages (e.g. before order book initialization from getProductOrderBook)
+            return
+        elif sequence > self.book.id + 1:
+            # missing packet restart
+            print 'restarting'
+            print self.book.id, sequence
+            #self.close()
+            # self.run()
+            return
+        self.book.id = sequence
 
         if _type == 'open':
-            pass
+            self.open_order(msg)
+        # market orders will not have a price field in a done msg
+        elif _type == 'done' and msg.get('price'):
+            self.done_order(msg)
         elif _type == 'match':
-            pass
-        elif type == 'change':
-            pass
+            self.match_order(msg)
+        # price is null for market orders in a change msg
+        elif type == 'change' and msg.get('price') and msg.get('new_size'):
+            self.change_order(msg)
 
-    def add_order(self):
-        pass
+    def parse_msg(self, message):
+        numeric_fields = ['price', 'size', 'old_size', 'new_size', 'remaining_size']
+        msg = json.loads(message)
+        msg = {k: float(v) if k in numeric_fields else v
+               for k, v in msg.iteritems()}
+        return msg
 
-    def remove_order(self):
-        pass
+    def _get_levels(self, side):
+        return self.book.bids if side == 'buy' else self.book.asks
 
-    def change_order(self):
-        pass
+    def open_order(self, msg):
+        side = msg['side']
+        price = msg['price']
+        size = msg['remaining_size']
+        order_id = msg['order_id']
 
+        levels = self._get_levels(side)
+        ob.add_order(levels, price, size, order_id)
+
+    def done_order(self, msg):
+        side = msg['side']
+        price = msg['price']
+        size = msg['remaining_size']
+        order_id = msg['order_id']
+
+        levels = self._get_levels(side)
+        ob.remove_order(levels, price, size, order_id)
+
+    def match_order(self, msg):
+        # the side field indicates the maker order side
+        side = msg['side']
+        price = msg['price']
+        size = msg['size']
+        order_id = msg['maker_order_id']
+        levels = self._get_levels(side)
+
+        ob.remove_order(levels, price, size, order_id)
+
+    def change_order(self, msg):
+        side = msg['side']
+        price = msg['price']
+        size = msg['new_size']
+        order_id = msg['order_id']
+        levels = self._get_levels(side)
+
+        ob.update_order(levels, price, size, order_id)
 
 
 if __name__ == '__main__':
