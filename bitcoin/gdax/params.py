85c1952fa3425dd336ed48989c8232817886ab71
Divir Gupta <divir94@gmail.com>
2017-08-20 01:13:06 -0400

    correct table name

--- bitcoin/gdax/params.py
+++ bitcoin/gdax/params.py
@@ -3,7 +3,7 @@ SNAPSHOT_TBLS = {
     'BTC-USD': 'GdaxBtcSnapshot'
 }
 MSG_TBLS = {
-    'BTC-USD': 'GdaxMessages'
+    'BTC-USD': 'GdaxBtcMessage'
 }
 MSG_COLS_TBL = ['sequence', 'time', 'received_time', 'type', 'price', 'size', 'order_id',  'side', 'order_type',
                 'funds', 'remaining_size', 'reason', 'trade_id', 'maker_order_id', 'taker_order_id', 'new_size',
