7ae061d7abffcd49fd0a9b80d8a5f74a426bff16
Divir Gupta <divir94@gmail.com>
2017-09-23 18:32:41 -0400

    execute orders

--- bitcoin/backtester/api.py
+++ bitcoin/backtester/api.py
@@ -7,11 +7,35 @@ class Backtester(object):
         self.product_id = product_id
         self.outstanding_orders = {}
         self.trades = {}
-        self.balances = {}
+        self.balance = {}
 
-    def execute_orders(self):
+    def execute_orders(self, orders):
         # update outstanding orders and balances
-        pass
+        for order in orders:
+            new_balance = self.update_balance(order)
+            self.balance = new_balance
+            self.outstanding_orders[order.id] = order
+
+    @staticmethod
+    def update_balance(order, balance):
+        new_balance = balance.copy()
+        if order.type == 'limit':
+            if order.side == 'buy':
+                value = order.price * order.size
+                current_balance = balance[order.base]
+                field = order.base
+            elif order.side == 'sell':
+                value = order.size
+                field = order.currency
+                current_balance = balance[order.currency]
+            else:
+                raise ValueError
+
+            assert current_balance >= value, 'Insufficient balance {} for order {}'.format(current_balance, order)
+            new_balance[field] -= value
+        else:
+            raise ValueError('Invalid order type {}'.format(order.type))
+        return new_balance
 
     def update_orders(self, msg):
         pass
@@ -24,5 +48,7 @@ class Backtester(object):
             new_orders = strategy.rebalance(message=msg,
                                             book=book,
                                             outstanding_orders=self.outstanding_orders,
-                                            balance=self.balances)
+                                            balance=self.balance)
+            self.execute_orders(new_orders)
+            self.update_orders(msg)
             book.process_message(msg)
